e6ac64d6b12b7a1b00db885c6fc734df35abd3f6
jayce.baxter <jayce.baxter@proton.me>
2025-11-02 21:04:26 -0500

    init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Add button
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // New button
+        private void BtnNew_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Update button
+        private void BtnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Remove button
+        private void BtnRemove_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Remove all button
+        private void BtnRemoveAll_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Exit button
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
