4a1fc10eef8ab6ff04c5012dc37c3a2a96da577f
jayce.baxter <jayce.baxter@proton.me>
2025-11-02 21:41:22 -0500

    added constructors and fixed bugs

---
+++ Service.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Service
+    {
+        private int count;
+        private int customerID;
+        private string firstName;
+        private string lastName;
+        private string phoneNumber;
+        private string vehicleMake;
+        private string vehicleModel;
+        private string vehicleColour;
+        private int vehicleYear;
+        private bool engOilChange;
+        private bool transOilChange;
+        private bool airFilterChange;
+        private decimal totalCost;
+
+        // Default constructor
+        public Service()
+        {
+            count = count + 1;
+
+            customerID = count;
+        }
+
+        // Parameterized constructor
+        // I'm using P to denote that it's a parameter instead of a variable name... so I don't get confused
+        // Rename later?
+        public Service (string pFirstName, string pLastName, string pPhoneNumber, string pVehicleMake, string pVehicleModel, string pVehicleColour, int pVehicleYear, bool pEngOilChange, bool pTransOilChange, bool pAirFilterChange, decimal pTotalCost)
+        {
+            firstName = pFirstName;
+            lastName = pLastName;
+            phoneNumber = pPhoneNumber;
+            vehicleMake = pVehicleMake;
+            vehicleModel = pVehicleModel;
+            vehicleColour = pVehicleColour;
+            vehicleYear = pVehicleYear;
+            engOilChange = pEngOilChange;
+            transOilChange = pTransOilChange;
+            airFilterChange = pAirFilterChange;
+            totalCost = pTotalCost;
+        }
+    }
+}
